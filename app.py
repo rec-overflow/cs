e46ab57a83dbf8e78cff602b48a5a0f4ee7fa1a0
rec-overflow <ppspasha@ukr.net>
2026-09-17 15:22:06 +0200

    add alternative host url and disable favicon

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def index():
         <html>
             <head>
                 <meta charset="utf-8">
-                <title>Message Board</title>
+                <link rel="icon" href="data:,">
+                <title>game codes</title>
             </head>
             <body>
                 <div style="position:absolute;left:50%;top:50%;translate:-50% -50%;">
@@ -22,6 +23,7 @@ def index():
                     <h2>Codes</h2>
                     {''.join('<p>' + msg + '</p>' for msg in messages)}
                     <a href="/delete">delete</a>
+                    <a href="https://pvl.pythonanywhere.com" style="position:absolute;bottom:1vh;right:1vw;">Change host</a>
                 </div>
             </body>
         </html>
